c4eed3890ad2fc54ef579d72801e129f46abf5fd
Schattsch <karelsch123@gmail.com>
2023-09-06 12:29:23 +0300

    Exercise 37: Printing out text

---
+++ src/com/karel/program/PrintText.java
@@ -0,0 +1,10 @@
+package com.karel.program;
+
+public class PrintText {public static void main(String[] args) {
+    printText();
+}
+
+    public static void printText() {
+        System.out.println("In the beginning there were the swamp, the hoe and Java.");
+    }
+}
